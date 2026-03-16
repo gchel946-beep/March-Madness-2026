b83f431f615d044089a9b22052736e2aa6f9f507
Graham Helmuth <gchel946@gmail.com>
2026-03-16 15:29:41 -0700

    fix config syntax

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,3 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */  
+const nextConfig = { reactStrictMode: true };  
+export default nextConfig; 
